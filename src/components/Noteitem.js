7e007cb058b391163e0514878a4e9a7bc6476f8d
Himanshu Verma <himanshuv242cp@gmail.com>
2023-02-04 01:22:55 +0530

    Add note and delete note funtion implemented

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext';
 
 const Noteitem = (props) => {
+  const context = useContext(noteContext);
+  const {deleteNote}=context;
     const {note}=props;
   return (
     <div className='col-md-3'>
@@ -8,6 +11,10 @@ const Noteitem = (props) => {
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
+          <div style={{ display: 'flex' }}>
+      <i className="fa-regular fa-pen-to-square" style={{ marginRight: 'auto' }} />
+      <i className="fa-solid fa-trash-can" onClick={()=>{deleteNote(note._id)}} />
+    </div>
         </div>
       </div>
     </div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -5,7 +5,7 @@ import NoteContext from "./noteContext";
 const NoteState = (props) => {
   const notesInitial = [
     {
-      _id: "63d6c2a77b62ad815afd32d5",
+      _id: "63d6c2a77ab62ad815afd32d5",
       user: "63cefc624c3f3b2ce17bf253",
       title: "study 2",
       description: "Please study",
@@ -14,7 +14,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "63dd250b4cf3d5462d04cf20",
+      _id: "63dd250b4chf3d5462d04cf20",
       user: "63cefc624c3f3b2ce17bf253",
       title: "Rich",
       description: "Work hard",
@@ -23,7 +23,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "63dd250b4cf3d5462d04cf20",
+      _id: "63dd250b4cf3d546g2d04cf20",
       user: "63cefc624c3f3b2ce17bf253",
       title: "Rich",
       description: "Work hard",
@@ -32,7 +32,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "63dd250b4cf3d5462d04cf20",
+      _id: "63dd250b4cf3d5462df04cf20",
       user: "63cefc624c3f3b2ce17bf253",
       title: "Rich",
       description: "Work hard",
@@ -41,7 +41,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "63dd250b4cf3d5462d04cf20",
+      _id: "63dd250b4cf3d5462dd04cf20",
       user: "63cefc624c3f3b2ce17bf253",
       title: "Rich",
       description: "Work hard",
@@ -52,8 +52,34 @@ const NoteState = (props) => {
   ];
 
   const[notes,setNotes]= useState(notesInitial)
+
+  //Add a Note
+  const addNote = (title,description,tag)=>{
+    console.log("Adding a new note");
+   const note={
+    _id: "63dd250b4cf3d5462ddf04cf20",
+    user: "63cefc624c3f3b2cef17bf253",
+    title: title,
+    description: description,
+    tag: tag,
+    date: "2023-02-03T15:15:23.355Z",
+    __v: 0,
+  };
+    setNotes(notes.concat(note))
+  }
+  //Delete a Note
+  const deleteNote = (id)=>{
+    console.log("Delete note with"+id);
+    const newNotes = notes.filter((note)=>{ return note._id!==id})
+    setNotes(newNotes)
+  }
+  
+  //Edit a Note
+  const editNote = (id, title, description, tag)=>{
+
+  }
   return (
-    <NoteContext.Provider value={{notes,setNotes}}>{props.children}</NoteContext.Provider>
+    <NoteContext.Provider value={{notes,addNote,deleteNote,editNote}}>{props.children}</NoteContext.Provider>
   );
 };
 
